b78e5113311372b92e13dde3a9968d35748071bf
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-22 12:20:19 +0530

    randome changes

---
+++ rustmemory/target/debug/deps/rustmemory-579f55cf2595ed92.d
@@ -0,0 +1,5 @@
+G:\rust\rustmemory\target\debug\deps\rustmemory-579f55cf2595ed92.d: src\main.rs
+
+G:\rust\rustmemory\target\debug\deps\rustmemory-579f55cf2595ed92.exe: src\main.rs
+
+src\main.rs:
---
+++ rustmemory/target/debug/rustmemory.d
@@ -0,0 +1,1 @@
+G:\rust\rustmemory\target\debug\rustmemory.exe: G:\rust\rustmemory\src\main.rs
