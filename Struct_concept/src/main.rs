1a0b44d19818bb81336aedd69c5ba2c6d368e73f
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-08-02 02:30:35 +0530

    feat: Implement command-line argument parsing and file reading

--- Struct_concept/src/main.rs
+++ Struct_concept/src/main.rs
@@ -20,27 +20,30 @@
 
 // pattern mathcing
 
-enum Shape {
-    Circle(f64),
-    Square(f64),
-    Rectangel(f64, f64),
-}
+// enum Shape {
+//     Circle(f64),
+//     Square(f64),
+//     Rectangel(f64, f64),
+// }
 
-fn calculate_data(Shape: Shape) -> f64 {
-    let result = match Shape {
-        Shape::Circle(radius) => 3.14 * radius * radius,
-        Shape::Rectangel(width, height) => width * height,
-        Shape::Square(side) => side * side,
-    };
-    return result;
-}
+// fn calculate_data(Shape: Shape) -> f64 {
+//     let result = match Shape {
+//         Shape::Circle(radius) => 3.14 * radius * radius,
+//         Shape::Rectangel(width, height) => width * height,
+//         Shape::Square(side) => side * side,
+//     };
+//     return result;
+// }
 
-fn main() {
-    let circle_data: Shape = Shape::Circle(3.4);
-    let square_data: Shape = Shape::Square(11.3);
-    let rectangel_data: Shape = Shape::Rectangel(2.3, 5.8);
+// fn main() {
+//     let circle_data: Shape = Shape::Circle(3.4);
+//     let square_data: Shape = Shape::Square(11.3);
+//     let rectangel_data: Shape = Shape::Rectangel(2.3, 5.8);
 
-    println!("the area of circle {:?}", calculate_data(circle_data));
-    println!("the area of square {:?}", calculate_data(square_data));
-    println!("the area of rectangle {:?}", calculate_data(rectangel_data));
-}
+//     println!("the area of circle {:?}", calculate_data(circle_data));
+//     println!("the area of square {:?}", calculate_data(square_data));
+//     println!("the area of rectangle {:?}", calculate_data(rectangel_data));
+// }
+
+
+// Error handling 
